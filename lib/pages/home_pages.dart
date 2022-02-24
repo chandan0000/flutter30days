177491166b1d3a12ea8b840bac12e194b3e0133d
chandan kumar <kumarchandan41u@gmail.com>
2022-02-24 17:32:45 +0530

    jsondecoder

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,11 +1,35 @@
+import 'dart:convert';
+
 import 'package:catlog/models/catalog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+  final String name = "Codepur";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
